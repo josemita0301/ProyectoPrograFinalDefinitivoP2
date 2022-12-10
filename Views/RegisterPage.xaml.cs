10eb9e50554fecf4afc3cfeccb65f54801a20af4
Amely Cordova <amely.cordova@udla.edu.ec>
2022-12-10 16:32:40 -0500

    RegisterPage

---
+++ Views/RegisterPage.xaml.cs
@@ -0,0 +1,15 @@
+namespace ProyectoPrograFinalDefinitivoP2.Views;
+
+public partial class RegisterPage : ContentPage
+{
+	public RegisterPage()
+	{
+		InitializeComponent();
+	}
+
+    private async void Register_Clicker(object sender, EventArgs e)
+    {
+        
+
+    }
+}
