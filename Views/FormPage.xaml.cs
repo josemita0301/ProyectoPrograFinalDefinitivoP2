54bc43a6c56db52a753414dcc7ea78de5c87eb8a
Anto <antonella.cortes@udla.edu.ec>
2022-12-11 00:49:05 -0500

    Enable checkbow to use button. Start option to work with email.

--- Views/FormPage.xaml.cs
+++ Views/FormPage.xaml.cs
@@ -1,4 +1,4 @@
-namespace ProyectoPrograFinalDefinitivoP2.Views;
+﻿namespace ProyectoPrograFinalDefinitivoP2.Views;
 
 public partial class FormPage : ContentPage
 {
@@ -7,11 +7,30 @@ public partial class FormPage : ContentPage
         InitializeComponent();
     }
 
-    private async void LearnMore_Clicked(object sender, EventArgs e)
+    private async void LearnMore_Clicked(object sender, EventArgs e);
     {
         // Navigate to the specified URL in the system browser.
-        await Launcher.Default.OpenAsync("https://aka.ms/maui");
+        //await Launcher.Default.OpenAsync("https://aka.ms/maui");
+
+    /**
+            if (Email.Default.IsComposeSupported)
+            {
+                var message = new EmailMessage
+                {
+                    Subject = "Hello!",
+                    Body = "Your adoption process has started! The current owner will get in touch soon.",
+                    BodyFormat = EmailBodyFormat.PlainText,
+                    To = new List<string>(new[] { "antocortesl27@gmail.com", "anto.27.cortes@gmail.com"})
+                };
+                message.Attachments.Add(new EmailAttachment(Path.Combine(FileSystem.CacheDirectory, "dotnet_bot.svg")));
+
+             await Email.Default.ComposeAsync(message);
+            }
+    **/
+        
     }
+
+
 
     private void OnCheckBoxCheckedChanged(object sender, CheckedChangedEventArgs e)
     {
