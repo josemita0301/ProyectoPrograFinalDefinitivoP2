830eccd412adc729ea385375f0fc315e1eda09f9
Jose Merlo <josemielmer2002@hotmail.com>
2022-12-11 00:26:19 -0500

    readfiles login

--- Views/Login.xaml.cs
+++ Views/Login.xaml.cs
@@ -1,5 +1,6 @@
 using Microsoft.Maui.ApplicationModel.Communication;
 using ProyectoPrograFinalDefinitivoP2.Models;
+using System.IO.IsolatedStorage;
 
 namespace ProyectoPrograFinalDefinitivoP2.Views;
 
@@ -13,8 +14,33 @@ public partial class Login : ContentPage
     private async void LoginButton(object sender, EventArgs e)
     {
         string appDataPath = FileSystem.AppDataDirectory;
-        await Navigation.PushModalAsync(new DogPage());
 
+        string[] archivos = Directory.GetFiles(appDataPath);
+            int cont = 0;
+        for (int i = 0; i < archivos.Length; i++)
+        {
+            string nombreArchivo = archivos[i];
+            foreach (string line in File.ReadLines(nombreArchivo))
+            {
+                if (cont == 0)
+                {
+                    if (UsernameField.Text == line)
+                    {
+
+                    }
+                }
+
+                if (cont == 1)
+                {
+                    if (PasswordField.Text == line)
+                    {
+
+                    }
+                }
+                cont++;
+            }
+        }
+            
     }
 
     async void RegisterButton(object sender, EventArgs e)
