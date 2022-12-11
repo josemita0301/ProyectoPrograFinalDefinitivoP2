da84878355bb3ce10a8854a2609420f542dc3e1c
Amely Cordova <amely.cordova@udla.edu.ec>
2022-12-11 16:32:12 -0500

    Send Mail New Owner

--- Views/FormPage.xaml.cs
+++ Views/FormPage.xaml.cs
@@ -1,4 +1,10 @@
-﻿namespace ProyectoPrograFinalDefinitivoP2.Views;
+﻿using ProyectoPrograFinalDefinitivoP2.Models;
+using System.Net.Mail;
+using System.Net.Security;
+using System.Net;
+using System.Security.Cryptography.X509Certificates;
+
+namespace ProyectoPrograFinalDefinitivoP2.Views;
 
 public partial class FormPage : ContentPage
 {
@@ -20,11 +26,7 @@ public partial class FormPage : ContentPage
 
     private async void LearnMore_Clicked(object sender, EventArgs e)
     {
-        // enviar mensaje de confirmación
-        {
-            string number = "+593 " + "987629927"; // LEER DE ARCHIVO
-            SendWhatsapp(number, "Hi! The adoption process has started!");
-        }
+        SendMailNewOwner();
 
     }
 
@@ -34,5 +36,24 @@ public partial class FormPage : ContentPage
         {
             submit.IsEnabled = true;
         }
+    }
+
+    public void SendMailNewOwner()
+    {
+        MailMessage correo = new MailMessage();
+        correo.From = new MailAddress("betterhomesender@gmail.com", "Better Home", System.Text.Encoding.UTF8);//Correo de salida
+        correo.To.Add("amelycordova49@gmail.com"); //Correo destino?
+        correo.Subject = "Correo de adopcion"; //Asunto
+        correo.Body = "Your request has been processed! The current owner will get in contact with you soon."; //Mensaje del correo
+        correo.IsBodyHtml = true;
+        correo.Priority = MailPriority.Normal;
+        SmtpClient smtp = new SmtpClient();
+        smtp.UseDefaultCredentials = false;
+        smtp.Host = "smtp.gmail.com"; //Host del servidor de correo
+        smtp.Port = 587; //Puerto de salida
+        smtp.Credentials = new System.Net.NetworkCredential("betterhomesender@gmail.com", "pzxmsztrhenlvjcu");//Cuenta de correo
+        ServicePointManager.ServerCertificateValidationCallback = delegate (object s, X509Certificate certificate, X509Chain chain, SslPolicyErrors sslPolicyErrors) { return true; };
+        smtp.EnableSsl = true;//True si el servidor de correo permite ssl
+        smtp.Send(correo);
     }
 }
